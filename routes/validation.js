e9171ccc08b81e9eb60ffcf483e1f0f0926f2608
Ming Tung Wong <mingtungwong@outlook.com>
2018-08-13 11:40:12 -0400

    Update validation.js
    Add timeout

--- routes/validation.js
+++ routes/validation.js
@@ -2,7 +2,7 @@ const ping = require('ping');
 const router = require('express').Router();
 
 router.get('/valid/:siteName', (req, res, next) => {
-    ping.promise.probe(req.params.siteName)
+    ping.promise.probe(req.params.siteName, { timeout: 3 })
     .then(result => res.send({alive: result.alive}));
 });
 
