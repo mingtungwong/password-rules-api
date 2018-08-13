7f603d87d36d2b44fa78624715a4e5112aa53f85
Ming Tung Wong <mingtungwong@outlook.com>
2018-08-13 11:43:35 -0400

    Update validation.js
    console logs

--- routes/validation.js
+++ routes/validation.js
@@ -3,7 +3,11 @@ const router = require('express').Router();
 
 router.get('/valid/:siteName', (req, res, next) => {
     ping.promise.probe(req.params.siteName, { timeout: 3 })
-    .then(result => res.send({alive: result.alive}));
+    .then(result => {
+        console.log(result)
+        res.send({alive: result.alive})
+    })
+    .catch(error => console.log(error));
 });
 
 module.exports = router;
