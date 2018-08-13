636fb6f2edc884da1da56aa0209fcc9c8dc8d32c
Ming <mingtungwong@outlook.com>
2018-08-13 12:04:54 -0400

    New pinging code

--- routes/validation.js
+++ routes/validation.js
@@ -1,13 +1,28 @@
-const ping = require('ping');
+const ping = require('net-ping');
 const router = require('express').Router();
 
 router.get('/valid/:siteName', (req, res, next) => {
-    ping.promise.probe(req.params.siteName, { timeout: 3 })
-    .then(result => {
-        console.log(result)
-        res.send({alive: result.alive})
+    const session = ping.createSession()
+    const pingPromise = new Promise((resolve, reject) => {
+        session.pingHost(req.params.siteName, (error, target) => {
+            if(error) {
+                reject(new Error(error instanceof ping.RequestTimedOutError ? 'Invalid site' : error.toString()))
+            } else resolve(target)
+        })
     })
-    .catch(error => console.log(error));
+    pingPromise.then(result => {
+        console.log(result)
+        res.status(200).send(null)
+    }).catch(error => {
+        console.log(error)
+        res.status(400).send(null)
+    })
+    // ping.promise.probe(req.params.siteName, { timeout: 3 })
+    // .then(result => {
+    //     console.log(result)
+    //     res.send({alive: result.alive})
+    // })
+    // .catch(error => console.log(error));
 });
 
 module.exports = router;
